9d9e3065b5d582431c7d4c18d70d231ba0bf0e44
sofi122d <sofialivnorre@gmail.com>
2023-09-25 18:32:00 +0200

    ret tekst

--- Components/HomeScreen.js
+++ Components/HomeScreen.js
@@ -8,7 +8,7 @@ export default function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{paddingTop: 40, fontWeight: 'bold'}}>Bestil mad</Text>
-      <Text style={{paddingTop: 40}}>Her sakl man kunne bestille den mad, til den tid eller ligende man gerne vil hente den</Text>
+      <Text style={{paddingTop: 40, padding: 40}}>Her skal man kunne bestille den mad, til den tid eller ligende man gerne vil hente den</Text>
       <Button title="Gå til indstillinger" onPress={() => navigation.navigate('Indstillinger')}/>
       <StatusBar style="auto" />
     </View>
